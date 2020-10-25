6965ef3673496ae6a231ea5bae77ad617dfb5174
alexKirchmann <tauva@ukr.net>
2020-10-25 15:16:48 +0200

    Repeating Background, going to add Sound

---
+++ RepeatBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour {
+    public float speed;
+    public float startX;
+    public float endX;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * (speed * Time.deltaTime));
+
+        if (transform.position.x <= endX)
+            transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+    }
+}
