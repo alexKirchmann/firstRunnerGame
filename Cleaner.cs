a6f008aa62268e4616d086f14db0be7549091845
alexKirchmann <tauva@ukr.net>
2020-10-25 16:16:26 +0200

    Object Cleaner + Score Counter basis, going to add UI

---
+++ Cleaner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleaner : MonoBehaviour {
+    public float endX;
+
+    void Update() {
+        if (transform.position.x <= endX)
+            Destroy(gameObject);
+        else {
+            Destroy(gameObject, 5);
+        }
+    }
+}
---
+++ ScoreCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour {
+    private int score;
+
+    private void OnTriggerExit2D (Collider2D other) {
+        if (other.CompareTag("Obstacle")) {
+            score++;
+        }
+    }
+}
